3c1a184c65a4dcc4fc9dea29f32fec5423bdb4c2
Homesh Nimbarte <75970868+resonating-homesh@users.noreply.github.com>
2022-12-24 17:54:54 +0530

    integrated signup api

--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -1,8 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
+
+  const [credentials, setCredentials] = useState({
+    name: "",
+    email: "",
+    password: ""
+  });
+
+  let history = useNavigate();
+
+  const onChange = (e) => {
+    e.preventDefault();
+    setCredentials({ ...credentials, [e.target.id]: e.target.value })
+  }
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    const { name, email, password } = credentials;
+    // Send a POST request to the /users/register route with the name, email, and password as query parameters
+    fetch('/users/register?name=' + name + '&email=' + email + '&password=' + password, { method: 'GET' })
+    .then(response => response.json())
+    .then(data => {
+      /// Check if the registration was successful
+      if (data.success) {
+        // Save the JWT authentication token in local storage
+        localStorage.setItem('jwt', data.jwt);
+        // Redirect to the chat page
+        // window.location.href = '/chat';
+        history("/chat");
+      } else {
+        // Display the error message
+        const errorMessage = document.getElementById('errorMessage');
+        errorMessage.style.display = 'block';
+      }
+    });
+  };
+
+  const handleLogin = () => {
+    history("/login");
+  };
+
   return (
     <div className="login">
       <div className="header">
@@ -29,15 +69,20 @@ const Login = () => {
         </p>
         <hr style={{ width: "90px" }} />
       </div>
-      <input className="email" placeholder="  Your Name" type="text"></input>
-      <input className="email" placeholder="  Email Adress" type="text"></input>
+      <form style={{display: "flex", flexDirection: "column", alignItems:"center", justifyContent:"center"}} onSubmit={handleClick}>
+      <input onChange={onChange} value={credentials.name} id="name"  className="email" placeholder="  Your Name" type="text" required></input>
+      <input onChange={onChange} value={credentials.email} id="email" className="email" placeholder="  Email Adress" type="text" required></input>
       <input
         className="password"
         placeholder="  Password"
         type="password"
+        required
+        id="password"
+        onChange={onChange}
+        value={credentials.password}
       ></input>
-      <button className="submit"> Signup </button>
-     
+      <button type="submit" className="submit"> Signup </button>
+      </form>
       <div className="row">
         <hr style={{ width: "90px" }} />
         <p
@@ -54,7 +99,7 @@ const Login = () => {
         <hr style={{ width: "90px" }} />
       </div>
       <p> Already have an account?</p>
-      <button className="submit" style={{marginBottom:"10px"}} > Login </button>
+      <button onClick={handleLogin} className="submit" style={{marginBottom:"10px"}} > Login </button>
     </div>
   );
 };
